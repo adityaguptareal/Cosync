5fd9036ea0cea22a494259b7899f77ecb821dc6b
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-05-13 21:14:55 +0530

    changes with webhook api

--- Server/Server.js
+++ Server/Server.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 // app.use('/api/v1/admin', userRoutes);
-app.use('/api/v1/user', userRoutes); // 
+app.use('/api/api/webhooks/v1/user', userRoutes); // 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
